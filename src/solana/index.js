2192bb972f552f3c66735228722f584350020052
tobiadiks <tobiadiks@gmail.com>
2022-01-02 02:42:34 +0100

    installed dependencies

---
+++ src/solana/index.js
@@ -0,0 +1,14 @@
+import Wallet from "@project-serum/sol-wallet-adapter";
+import {
+  Connection,
+  SystemProgram,
+  Transaction,
+  PublicKey,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import { deserialize, serialize } from "borsh";
+
+const cluster = "https://api.devnet.solana.com";
+const connection = new Connection(cluster, "confirmed");
+const wallet = new Wallet("https://www.sollet.io", cluster);
+const programId = new PublicKey("Evpd75qp5hJFbgfNnLQ4K5gL3g1HhwJfC7HPmEUZrWwM");
